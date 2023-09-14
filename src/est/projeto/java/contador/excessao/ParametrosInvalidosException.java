290f764a986ce26d3c6baf914d580ef103c8e1f4
Queel Gomes <124888102+queelgomes@users.noreply.github.com>
2023-09-14 09:26:58 -0300

    Add files via upload

---
+++ src/est/projeto/java/contador/excessao/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package est.projeto.java.contador.excessao;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String msg) {
+        super(msg);
+    }
+}
